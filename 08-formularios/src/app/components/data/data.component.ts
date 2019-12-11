9777256e170d43b379757be078c2f9dc22837396
Milton Montes <miltonmontes17@gmail.com>
2019-12-10 21:39:57 -0500

    Continuación Formularios Data

--- 08-formularios/src/app/components/data/data.component.ts
+++ 08-formularios/src/app/components/data/data.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validator, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -8,21 +8,68 @@ import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
 })
 export class DataComponent {
 
-
   forma: FormGroup;
 
+  usuario: object = {
+    nombreCompleto: {
+      nombre: 'Milton',
+      apellido: 'Montes'
+    },
+    correo: 'miltonmontes17@gmail.com',
+    //pasatiempos: ['Comer', 'Dormir', 'Jugar']
+  };
+
   constructor() {
+    console.log(this.usuario);
     this.forma = new FormGroup({
-      nombre: new FormControl('', Validators.required),
-      apellido: new FormControl('', Validators.required),
+
+      nombreCompleto: new FormGroup({
+
+        nombre: new FormControl('', [Validators.required,
+        Validators.minLength(3)
+        ]),
+        apellido: new FormControl('', [Validators.required,
+                                      this.noMontes
+                                    ])
+      }),
       correo: new FormControl('', [Validators.required,
-                                    Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
-                                  ]),
+      Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+      ]),
+      pasatiempos: new FormArray([
+        new FormControl('Comer', Validators.required)
+      ])
+
     });
+
+    //this.forma.reset(this.usuario);
+
+  }
+
+  agregarPasatiempo() {
+    (<FormArray>this.forma.controls['pasatiempos']).push(
+      new FormControl('', Validators.required)
+    );
+  }
+
+  noMontes(control: FormControl): { [s: string]: boolean } {
+    if (control.value === 'montes') {
+      return {
+        nomontes: true
+      }
+    }
+    return null;
   }
 
   guardarCambios() {
     console.log(this.forma.value);
     console.log(this.forma);
+
+    // this.forma.reset({
+    //   nombreCompleto: {
+    //     nombre: '',
+    //     apellido: ''
+    //   },
+    //   correo: ''
+    // });
   }
 }
